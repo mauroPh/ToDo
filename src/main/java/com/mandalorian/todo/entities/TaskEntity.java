7d4e9a2edb4bacbce8d6bc1787e20b478dd06d4f
mauro-mandalorian's <mauro.santos@esx.com.br>
2023-09-26 17:22:47 -0300

    database h2,profile de teste, JPA

--- src/main/java/com/mandalorian/todo/entities/TaskEntity.java
+++ src/main/java/com/mandalorian/todo/entities/TaskEntity.java
@@ -3,10 +3,19 @@ package com.mandalorian.todo.entities;
 import java.io.Serializable;
 import java.util.Objects;
 
-public class TaskEntity implements Serializable{
-	
-	private static final long serialVersionUID = 1L;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name = "tb_task")
+public class TaskEntity implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
     private String description;
